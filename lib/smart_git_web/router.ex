b7899ce0a4219d588fdad3ab36eb83026bb8da7a
Gustavo Oliveira <g.92oliveira@gmail.com>
2022-03-29 19:05:19 -0300

    ignore more informations

--- lib/smart_git_web/router.ex
+++ lib/smart_git_web/router.ex
@@ -10,9 +10,9 @@ defmodule SmartGitWeb.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
+  # pipeline :api do
+  #   plug :accepts, ["json"]
+  # end
 
   scope "/", SmartGitWeb do
     pipe_through :browser
@@ -32,6 +32,7 @@ defmodule SmartGitWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
+  # coveralls-ignore-start
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
@@ -52,4 +53,5 @@ defmodule SmartGitWeb.Router do
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
+  # coveralls-ignore-stop
 end
